ed29f733f4e3d9d55e3aeb88e4922bb5507a8a09
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-08-12 15:30:25 -0600

    finishes ancestor problem'

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,25 +1,50 @@
+#helper graph class, don't need it all and the search has to be different
 class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
-    def add_vertex(self, vertex):
-        """
-        Add a vertex to the graph.
-        """
-        self.vertices[vertex] = set()
+
+    def add_vertex(self, vertex_id):
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
+
+    #adds a directed edge so that inheritance is represented
     def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        """
         if v1 in self.vertices and v2 in self.vertices:
             self.vertices[v1].add(v2)
-        else:
-            raise IndexError("That vertex does not exist.")
+
 
 
 def earliest_ancestor(ancestors, starting_node):
-    #needs to take in a node
-    #and return the "oldest" connected node
-    #makes sense to do depth first search
+    #makes a directed graph
+    graph_of_ancestors = Graph()
+    #fills it with stuff
+    for pair in ancestors:
+        graph_of_ancestors.add_vertex(pair[0])
+        graph_of_ancestors.add_vertex(pair[1])
+        graph_of_ancestors.add_edge(pair[1], pair[0])
+    #sets up variables for the longest path that we've seen and the node at the end of that path
+    max_path_len = 1
+    #needs to return -1 if no parents are available
+    earliest_ancestor = -1
+    #queue for breadth first search
+    queue = []
+    #breadth first search, slight changes for deepest found node
+    queue.append([starting_node])
+    while len(queue) > 0:
+        path = queue.pop(0)
+        v = path[-1]
+        if (len(path) == max_path_len and v < earliest_ancestor) or (len(path) > max_path_len):
+            earliest_ancestor = v
+            max_path_len = len(path)
+        for neighbor in graph_of_ancestors.vertices[v]:
+            path_copy = list(path)
+            path_copy.append(neighbor)
+            queue.append(path_copy)
+    return earliest_ancestor
+
+
+
+
+
 
 
