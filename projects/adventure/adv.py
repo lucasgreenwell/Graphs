dd293f3f0510bde1d6798a21ad6dea80de2ada97
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-08-14 10:15:11 -0600

    finishes sprint challenge in 997 moves, can't optimize further

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -27,8 +27,42 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
 
+#This is kind of breadth first search but a bit modified
+#keeps track of final path
+traversal_path = []
+#keeps track of what to search in the graph next
+stack = []
+#keeps track of where I've already been
+rooms_visited = set()
+#start where you at
+stack.append(player.current_room.id)
+
+#until you've seen every room
+while len(rooms_visited) < len(room_graph):
+    current_room = stack[-1]
+    rooms_visited.add(current_room)
+    #dict of all the rooms adjacent and the direction they're in
+    neighbors = room_graph[current_room][1]
+    unvisited_neighbors = []
+    #loop through the neighboring rooms
+    for direction, room_num in neighbors.items():
+        #and add any that you haven't visited to the list
+        if room_num not in rooms_visited:
+            unvisited_neighbors.append((room_num, direction))
+    #if you got rooms that haven't been explored that you can go to
+    if len(unvisited_neighbors) > 0:
+        #go to one of them
+        stack.append(unvisited_neighbors[0][0])
+        traversal_path.append(unvisited_neighbors[0][1])
+    else:
+        #take the top thing off the stack
+        stack.pop()
+        for direction, room_num in neighbors.items():
+            #find the room you gotta go to next
+            if room_num == stack[-1]:
+                #and add its command to the list
+                traversal_path.append(direction)
 
 
 # TRAVERSAL TEST
